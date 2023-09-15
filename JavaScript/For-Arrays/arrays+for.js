70e68650817dc197b4fb60cb1eb2f78274a59529
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-09-15 14:29:02 -0300

    array

--- JavaScript/For-Arrays/arrays+for.js
+++ JavaScript/For-Arrays/arrays+for.js
@@ -3,16 +3,16 @@ const prompt = require("readline-sync");
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //for (let i = 0; i < arr.length; i++) {
- // console.log("indice", i, "- conteudo:", arr[i]);
+// console.log("indice", i, "- conteudo:", arr[i]);
 //}
 //console.clear();
 //for of
 
 ///for (const elemento of arr) {
 //  console.log(elemento);
-//} //percorre todo array
+//} //percorre os elementos
 //for in
 
-for (const indice in arr){
-  console.log(indice)
-}//mostra os indices
+for (const indice in arr) {
+  console.log(indice);
+} //percorre os indices
