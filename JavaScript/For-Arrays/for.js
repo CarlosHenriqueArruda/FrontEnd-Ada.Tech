29583b487f56dfec2ba75a20aadfd61df60a589d
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-09-15 13:21:48 -0300

    exercicio for

---
+++ JavaScript/For-Arrays/for.js
@@ -0,0 +1,27 @@
+const prompt = require("readline-sync");
+// let i;
+// primeiro onde inicia
+//segundo é até onde continua
+//controle
+//for (let i=0;i<5;i++){
+//}
+
+//let maiorNum = 0;
+//let numeroInformado;
+
+//for (let i = 1; i<= 5; i++){
+//numeroInformado = Number(prompt.question("informe um numero: "))
+// if (numeroInformado > maiorNum){
+// maiorNum = numeroInformado;
+// }
+//}
+//console.log(maiorNum)//
+
+//percorendo string com for
+
+const nome = "Carlos";
+//console.log(nome.length); //tamanho da string
+
+for (let i = 0; i < nome.length; i++) {
+  console.log(nome[i]);
+}
