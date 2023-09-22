915a0aa0f56915db9b6d1aefbbab8bf6946b5098
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-09-22 18:04:25 -0300

    adicionando elementos

--- TypeScript, POO e Browser APIs/Browser APIs/DOM API 1/js/index.js
+++ TypeScript, POO e Browser APIs/Browser APIs/DOM API 1/js/index.js
@@ -13,17 +13,35 @@ console.log(paragrafo);
 
 const emailinput = document.getElementsByName("email");
 
-console.log(emailinput)
+console.log(emailinput);
 
 //pegando elementos pelo ID da tag
 
-const imgjslogo = document.getElementById('js-logo')
+const imgjslogo = document.getElementById("js-logo");
 
-console.log(imgjslogo)
+console.log(imgjslogo);
 
 //query selector
 
-const imagem = document.querySelector('body > img')
+const imagem = document.querySelector("body > img");
 
-console.log(imagem)
+console.log(imagem);
+
+console.clear();
+
+//acessando e/ou alterando os conteudos das tags
+const primeiroP = document.querySelector("p.paragrafo");
+
+console.log(primeiroP);
+
+console.log("textContent:", primeiroP.textContent); //pegando o conteúdo em texto (ignora as tags exemplo "strong")
+console.log("innerHTML: ", primeiroP.innerHTML); //pega o conteudo completo, em HTML (incluindo tags)
+
+primeiroP.textContent = "Outra coisa"; //modifiquei o conteudo, não aceita tags, coloca em texto
+
+//value
+
+console.clear();
+
+emailinput[0].value = "carlos@email.com"; //get retorna um array, tem que colocar a posição
 
